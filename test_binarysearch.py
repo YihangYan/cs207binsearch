6b90b1c28a9b02c67ab723feb2085e2bf249ee59
Yihang Yan <yihang_yan@g.harvard.edu>
2016-09-27 16:53:33 -0400

    update test

--- test_binarysearch.py
+++ test_binarysearch.py
@@ -3,5 +3,34 @@ from pytest import raises
 import numpy as np
 from binarysearch import binary_search
 
-def test_simple():
-	assert binary_search([1,2,np.inf], 2) == 1
+empty_list = []
+sorted_num_list = list(range(15))
+combine_list = [3, 6, True, "combine", -2, "list"]
+'''
+Phase 1 - Simple test (interface illustration)
+'''
+def test_found():
+	assert binary_search(sorted_num_list, 2) == 2
+
+def test_found2():
+	assert binary_search(sorted_num_list, 6, 2, 7) == 6
+
+def test_notfound():
+	assert binary_search(sorted_num_list, 9.5) == -1
+
+def test_notfound2():
+	assert binary_search(sorted_num_list, 8, 2, 7) == -1
+
+'''
+Phase 2 - Test different input
+'''
+
+
+'''
+Phase 3 - Test Needles
+'''
+
+
+'''
+Phase 4 - Integration of previous test
+'''
