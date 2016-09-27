c292daa661db277c91c7c532556964586d9cbfcd
Yihang Yan <yihang_yan@g.harvard.edu>
2016-09-27 16:11:21 -0400

    update test

--- test_binarysearch.py
+++ test_binarysearch.py
@@ -1,6 +1,7 @@
+import unitest
 from pytest import raises
 import numpy as np
 from binarysearch import binary_search
 
 def test_simple():
-	assert binary_search([1,2,np.inf], 2) == 6
+	assert binary_search([1,2,np.inf], 2) == 1
