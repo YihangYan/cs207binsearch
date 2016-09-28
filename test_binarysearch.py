d98585953308b824a45383064f412c789d334c2f
Yihang Yan <yihang_yan@g.harvard.edu>
2016-09-27 21:48:06 -0400

    update test

--- test_binarysearch.py
+++ test_binarysearch.py
@@ -11,7 +11,17 @@ combine_list = [3, 6, True, "combine", -2, "list"]
 '''
 Phase 1 - Simple test (interface illustration)
 '''
+def test_found():
+	assert binary_search(sorted_num_list, 2) == 2
 
+def test_found2():
+	assert binary_search(sorted_num_list, 6, 2, 7) == 6
+
+def test_notfound():
+	assert binary_search(sorted_num_list, 9.5) == -1
+
+def test_notfound2():
+	assert binary_search(sorted_num_list, 8, 2, 7) == -1
 
 '''
 Phase 2 - Test different input
